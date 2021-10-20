d411a69b4842b3e50d8e4bd2b07758bed954f614
maikaedberg <69947279+maikaedberg@users.noreply.github.com>
2021-10-20 14:29:37 +0200

    changed Filterlit to return Maybe type

--- Solver/MySolver.hs
+++ Solver/MySolver.hs
@@ -21,15 +21,18 @@ solve (c:clauses)
         cond_x    = condition cond (c:clauses)
         cond_negx = condition (negLit cond) (c:clauses)
 
-filterLit :: Lit -> Cls -> [Cls]
+filterLit :: Lit -> Cls -> Maybe Cls
 filterLit l c
-    | elem l (literals c)          = [] -- literal found in clause
-    | elem (negLit l) (literals c) = [BigOr (filter (/= (negLit l)) (literals c))] -- neg literal found in clause
-    | otherwise                    = [c]
+    | elem l (literals c)          = Nothing -- literal found in clause
+    | elem (negLit l) (literals c) = Just (BigOr (filter (/= (negLit l)) (literals c))) -- neg literal found in clause
+    | otherwise                    = Just c
 
 condition :: Lit -> [Cls] -> [Cls]
 condition l [] = []
-condition l (c:cs) = (filterLit l c) ++ (condition l cs)
+condition l (c:cs) = 
+    case (filterLit l c) of
+        Nothing -> condition l cs
+        Just filtered ->  filtered:(condition l cs)
 
 solution :: CNF -> Maybe Subst
 solution cnf = solve (clauses cnf)
